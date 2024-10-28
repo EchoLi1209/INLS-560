7955ce22dfe6c28ce4e09a604e77e91f66b36994
EchoLi1209 <echo1209@ad.unc.edu>
2024-10-28 19:08:08 -0400

    Add files via upload

---
+++ assignment_04/na_assignment4.py
@@ -0,0 +1,18 @@
+#constants for Skate Coach
+MIN_YEARS_SKATING = 10
+MIN_YEARS_COACHING = 2
+
+years_skating = int(input('Enter your years of skating experience:'))
+years_coaching = int(input('Enter how many years you have been a skating coach:'))
+
+if years_skating >= MIN_YEARS_SKATING and years_coaching >= MIN_YEARS_COACHING:
+    print('Congratulations! You are eligible for the Skating Coach Position.')
+else:
+# Multiple-line with f-string
+    print(f'''
+Sorry, you do not meet the requirements for the Skating Coach Position,
+    
+You need at least:
+- {MIN_YEARS_SKATING} years of skating experience
+- {MIN_YEARS_TOP_AWARD} years as a skating coach
+''')
